27d3191ed8a9f8ae52c36eefc9550202548874a6
Megha03-coder <meghachauhan310@gmail.com>
2026-01-26 19:53:39 +0530

    number data type

---
+++ number.js
@@ -0,0 +1,4 @@
+var num1 = 20;
+var num2 = 3;
+var total = num1 + num2;
+console.log(total);
---
+++ number.ts
@@ -0,0 +1,4 @@
+var num1:number =20;
+var num2:number = 3;
+var total:number = num1 + num2;
+console.log(total);
