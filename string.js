57f433a79211c00878ab7a7bbe7aa454a8b9f0eb
Megha03-coder <meghachauhan310@gmail.com>
2026-01-26 20:03:37 +0530

    conversion num to string

--- string.js
+++ string.js
@@ -1,3 +1,7 @@
 var name1 = "megha";
 var name2 = "chauhan";
 console.log(name1 + name2);
+// number to string conversion
+var age = 20;
+console.log(age.toString());
+console.log(name1 + age);
--- string.ts
+++ string.ts
@@ -1,3 +1,10 @@
 var name1:string="megha";
 var name2:string="chauhan";
 console.log(name1+name2);
+
+
+// number to string conversion
+
+var age:number=20;
+console.log(age.toString());
+console.log(name1+age);
