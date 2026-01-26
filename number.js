74f0797d433d1bb6ce4101a6ece6d49843d40339
Megha03-coder <meghachauhan310@gmail.com>
2026-01-26 20:07:17 +0530

    conversion string to number

--- number.js
+++ number.js
@@ -2,3 +2,7 @@ var num1 = 20;
 var num2 = 3;
 var total = num1 + num2;
 console.log(total);
+// conversion string to number
+var name1 = "megha";
+var converter = Number(name1);
+console.log(converter);
--- number.ts
+++ number.ts
@@ -2,3 +2,9 @@ var num1:number =20;
 var num2:number = 3;
 var total:number = num1 + num2;
 console.log(total);
+
+// conversion string to number
+
+var name1:string="megha";
+var converter:number=Number(name1);
+console.log(converter);
